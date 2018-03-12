fc9e5f94204e00f75d0acb51298218c816ef9030
Andrius <andrius@wikiled.com>
2018-03-12 12:46:12 +0000

    Add native methods

---
+++ src/Wikiled.Console/hELPERS/NativeMethods.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Wikiled.Console.Helpers
+{
+    public static class NativeMethods
+    {
+        public const uint ES_CONTINUOUS = 0x80000000;
+
+        public const uint ES_SYSTEM_REQUIRED = 0x00000001;
+
+        [DllImport("kernel32.dll")]
+        public static extern uint SetThreadExecutionState(uint esFlags);
+    }
+}
