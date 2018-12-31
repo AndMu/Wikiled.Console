87a427feb97bfffc6402a640c70d953d9e798c52
Andrius Mudinas <keistokas@gmail.com>
2018-12-31 00:41:25 +0000

    Adding tests

---
+++ src/Wikiled.Console.Tests/Arguments/SyncExecutorTests.cs
@@ -0,0 +1,38 @@
+using System;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.Logging.Abstractions;
+using Wikiled.Console.Arguments;
+using Wikiled.Console.Tests.Data;
+
+namespace Wikiled.Console.Tests.Arguments
+{
+    [TestFixture]
+    public class SyncExecutorTests
+    {
+        private AutoStarter autoStarter;
+
+        private SyncExecutor instance;
+
+        [SetUp]
+        public void SetUp()
+        {
+            autoStarter = new AutoStarter(new NullLoggerFactory(), "Test", new[] { "One", "-Data=Test" });
+            autoStarter.RegisterCommand<SampleCommand, ConfigOne>("One");
+            autoStarter.RegisterCommand<SampleCommandTwo, ConfigTwo>("Two");
+            instance = CreateSyncExecutor();
+        }
+
+        [Test]
+        public void Construct()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SyncExecutor(null));
+        }
+
+        private SyncExecutor CreateSyncExecutor()
+        {
+            return new SyncExecutor(autoStarter);
+        }
+    }
+}
