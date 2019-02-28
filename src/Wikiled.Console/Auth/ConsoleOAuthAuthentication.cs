95917013b75a62f893225166f309c01efedbcc1c
Andrius Mudinas <andrius@wikiled.com>
2019-02-28 14:42:34 +0000

    Adding console OAuth authenticator

---
+++ src/Wikiled.Console/Auth/ConsoleOAuthAuthentication.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Wikiled.Common.Utilities.Auth;
+
+namespace Wikiled.Console.Auth
+{
+    public class ConsoleOAuthAuthentication<T> : IAuthentication<T>
+        where T : class
+    {
+        private readonly IAuthClient<T> client;
+
+        private readonly OAuthHelper helper;
+
+        public ConsoleOAuthAuthentication(IAuthClient<T> client, OAuthHelper helper)
+        {
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+            this.helper = helper ?? throw new ArgumentNullException(nameof(helper));
+        }
+
+        public async Task<T> Authenticate()
+        {
+            var auth = client.BuildAuthorizeUrl(helper.RedirectUri);
+            await helper.Start(auth, null).ConfigureAwait(false);
+            var code = helper.Code;
+            T token = await client.GetToken(code, helper.RedirectUri);
+            return token;
+        }
+
+        public Task<T> Refresh(T old)
+        {
+            return client.RefreshToken(old);
+        }
+    }
+}
---
+++ src/Wikiled.Console/Auth/IAuthClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Wikiled.Console.Auth
+{
+    public interface IAuthClient<T>
+        where T : class
+    {
+        string BuildAuthorizeUrl(string callback = null);
+
+        Task<T> GetToken(string code, string redirectUri = null);
+
+        Task<T> RefreshToken(T token);
+    }
+}
